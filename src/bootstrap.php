fc0f41abf3522b70bce4cd99917378d89460cd56
Jasmin-potager <jasmin@lepotager.org>
2026-09-05 16:33:37 +0200

    Load work catalog service

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 $config = require dirname(__DIR__) . '/config/app.php';
 require_once __DIR__ . '/Database.php';
 require_once __DIR__ . '/Search.php';
+require_once __DIR__ . '/WorkCatalog.php';
 
 $pdo = Database::connect($config);
 
