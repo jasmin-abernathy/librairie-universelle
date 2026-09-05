6d1e1cb78ee21efc59e4e572c9e939f869d3356d
Jasmin-potager <jasmin@lepotager.org>
2026-09-05 16:53:08 +0200

    Load catalog connector classes

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,6 +6,10 @@ $config = require dirname(__DIR__) . '/config/app.php';
 require_once __DIR__ . '/Database.php';
 require_once __DIR__ . '/Search.php';
 require_once __DIR__ . '/WorkCatalog.php';
+require_once __DIR__ . '/CatalogSource.php';
+require_once __DIR__ . '/HttpClient.php';
+require_once __DIR__ . '/BnfSruSource.php';
+require_once __DIR__ . '/CatalogImporter.php';
 
 $pdo = Database::connect($config);
 
