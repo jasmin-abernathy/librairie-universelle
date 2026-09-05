56929bf1c9fc34c15a8417d1006753cd7bc0d014
Jasmin-potager <jasmin@lepotager.org>
2026-09-05 18:42:50 +0200

    Load optional Moselle availability client

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -15,6 +15,7 @@ require_once __DIR__ . '/GallicaImporter.php';
 require_once __DIR__ . '/Isbn.php';
 require_once __DIR__ . '/OfferSource.php';
 require_once __DIR__ . '/OfferImporter.php';
+require_once __DIR__ . '/MoselleAvailabilityClient.php';
 require_once __DIR__ . '/Csrf.php';
 require_once __DIR__ . '/UploadValidator.php';
 require_once __DIR__ . '/SelfPublishingService.php';
